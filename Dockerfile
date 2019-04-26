742dd6529ee547a0e63ffbedb0232a2b48f5a958
Quentin Le Graverend <quentin@legraverend.fr>
2019-04-26 16:18:48 +0000

    some work in progress

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM ubuntu
 
 RUN apt-get update\
- && apt-get install jq vim\
+ && apt-get install jq vim git\
+ && git clone https://github.com/Kuaaaly/backend-engineer-test.git
--- exercise.sh
+++ exercise.sh
@@ -15,8 +15,33 @@ hello_world () {
     echo "Hello World!"
 }
 
+read_freelancer_data() {
+    i=0
+    while [[ $i -lt 5 ]]
+    do
+	if [[ is_there_another_exp ]]
+	then
+	    echo "C'est fini !"
+	else
+            jq ".freelance.professionalExperiences[$i]" examples/freelancer.json
+	fi
+	i=$i+1
+    done	
+}
+
+is_there_another_exp() {
+    nl='
+    '
+    if [[ $(jq ".freelance.professionalExperiences[$1]" examples/freelancer.json) != *$nl*  ]]
+    then
+	return 0
+    else
+	return 1
+    fi
+}
+
 # MAIN
 hello_world
-read_data
+read_freelancer_data
 
 
