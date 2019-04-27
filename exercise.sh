ee12c6f5bfb02cfc94ab0418885c2cd2dd2169f1
Quentin Le Graverend <quentin@legraverend.fr>
2019-04-27 12:11:25 +0000

    refactor function for printing associative array (debug purpose)

--- exercise.sh
+++ exercise.sh
@@ -15,7 +15,7 @@ declare -A SKILL_DURATION_LIST
 
 # FUNCTIONS
 another_element() {
-    nl='
+    local nl='
     '
     element=$(jq "$1" examples/freelancer.json)
     if [[ $element != "null" ]]
@@ -35,14 +35,14 @@ element_in_array() {
 }
 
 get_skill_list() {
-    i=0
+    local i=0
     while another_element .freelance.professionalExperiences[$i]
     do
-	j=0
+	local j=0
 	while another_element .freelance.professionalExperiences[$i].skills[$j]
 	do
-	    SKILL_NAME=$(jq -r ".freelance.professionalExperiences[$i].skills[$j].name" $INPUT_FILE)
-	    SKILL_ID=$(jq -r ".freelance.professionalExperiences[$i].skills[$j].id" $INPUT_FILE)
+	    local SKILL_NAME=$(jq -r ".freelance.professionalExperiences[$i].skills[$j].name" $INPUT_FILE)
+	    local SKILL_ID=$(jq -r ".freelance.professionalExperiences[$i].skills[$j].id" $INPUT_FILE)
             SKILL_LIST[$SKILL_NAME]=$SKILL_ID
 	    SKILL_DURATION_LIST[$SKILL_NAME]=0
 	    j=$j+1
@@ -52,13 +52,13 @@ get_skill_list() {
 }
 
 get_skill_duration() {
-    i=0
+    local i=0
     while another_element .freelance.professionalExperiences[$i]
     do
-        j=0
+        local j=0
         while another_element .freelance.professionalExperiences[$i].skills[$j]
         do
-            SKILL_NAME=$(jq -r ".freelance.professionalExperiences[$i].skills[$j].name" $INPUT_FILE)
+            local SKILL_NAME=$(jq -r ".freelance.professionalExperiences[$i].skills[$j].name" $INPUT_FILE)
 	    SKILL_DURATION_LIST[$SKILL_NAME]=$(( ${SKILL_DURATION_LIST[$SKILL_NAME]} + $(compute_duration ".freelance.professionalExperiences[$i]") )) 
             j=$j+1
         done
@@ -66,12 +66,35 @@ get_skill_duration() {
     done
 }
 
-print_duration_list_array() {
-    for KEY in "${!SKILL_DURATION_LIST[@]}"
-    do
-        # Print the KEY value
-        echo "$KEY : ${SKILL_DURATION_LIST[$KEY]}"
+print_associative_array() {
+    #Source : https://stackoverflow.com/questions/4069188/how-to-pass-an-associative-array-as-argument-to-a-function-in-bash
+    var=$(declare -p "$1")
+    eval "declare -A _arr="${var#*=}
+    for k in "${!_arr[@]}"; do
+        echo "$k: ${_arr[$k]}"
     done
+}
+
+get_skill_duration_non_overlap() {
+    local i=0
+    local SKILL_NAME=$1
+    declare -A EXP_DURATION_LIST
+    while another_element .freelance.professionalExperiences[$i]
+    do
+        local j=0
+        while another_element .freelance.professionalExperiences[$i].skills[$j]
+        do
+            if [[ $(jq -r ".freelance.professionalExperiences[$i].skills[$j].name" $INPUT_FILE) = $SKILL_NAME ]]
+	    then
+                START_DATE=$(jq -r ".freelance.professionalExperiences[$i].startDate" $INPUT_FILE)
+                END_DATE=$(jq -r ".freelance.professionalExperiences[$i].endDate" $INPUT_FILE)
+		EXP_DURATION_LIST[$START_DATE]=$END_DATE
+	    fi
+            j=$j+1
+        done
+        i=$i+1
+    done
+    print_associative_array "EXP_DURATION_LIST"
 }
 
 compute_duration() {
@@ -84,4 +107,7 @@ compute_duration() {
 # MAIN
 get_skill_list
 get_skill_duration
-print_duration_list_array
+get_skill_duration_non_overlap "Javascript"
+print_associative_array "SKILL_LIST"
+print_associative_array "SKILL_DURATION_LIST"
+
