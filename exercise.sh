25c75611d43c91a4cb5f7d1ab788f30a90cf111e
Quentin Le Graverend <quentin@legraverend.fr>
2019-04-27 15:12:17 +0000

    display result is ope

--- exercise.sh
+++ exercise.sh
@@ -26,28 +26,59 @@ another_element() {
     fi
 }
 
-element_in_array() {
-    # Source: https://stackoverflow.com/questions/3685970/check-if-a-bash-array-contains-a-value
-    local e match="$1"
-    shift
-    for e; do [[ "$e" == "$match" ]] && return 0; done
-    return 1
+compute_duration() {
+    var=$(declare -p "$1")
+    eval "declare -A _arr="${var#*=}
+    local i=0
+    local DURATION=0
+    KEYS=(${!_arr[@]})
+    while [[ i -lt ${#_arr[@]} ]]
+    do
+	DURATION=$(( $DURATION + (($(date --date=${_arr[${KEYS[$i]}]} +%s) - $(date --date=${KEYS[$i]} +%s) )/(60*60*24*30)) ))
+        i=$i+1
+    done
+    echo $DURATION
 }
 
-get_skill_list() {
-    local i=0
-    while another_element .freelance.professionalExperiences[$i]
+compute_overlap() {
+    var=$(declare -p "$1")
+    eval "declare -A _arr="${var#*=}
+    local i=1
+    local OVERLAP_DURATION=0
+    KEYS=(${!_arr[@]})
+    while [[ i -lt ${#_arr[@]} ]]
     do
-	local j=0
-	while another_element .freelance.professionalExperiences[$i].skills[$j]
-	do
-	    local SKILL_NAME=$(jq -r ".freelance.professionalExperiences[$i].skills[$j].name" $INPUT_FILE)
-	    local SKILL_ID=$(jq -r ".freelance.professionalExperiences[$i].skills[$j].id" $INPUT_FILE)
-            SKILL_LIST[$SKILL_NAME]=$SKILL_ID
-	    SKILL_DURATION_LIST[$SKILL_NAME]=0
-	    j=$j+1
-	done
-        i=$i+1
+	OVERLAP=$(( ($(date --date=${KEYS[${i}]} +%s) - $(date --date=${_arr[${KEYS[$((i-1))]}]} +%s) )/(60*60*24*30) ))
+	if [[ $OVERLAP -lt 0 ]]
+	then
+		OVERLAP_DURATION=$((OVERLAP_DURATION + OVERLAP))
+	fi
+	i=$i+1
+    done
+    echo $OVERLAP_DURATION
+}
+
+display_result() {
+    KEYS=(${!SKILL_LIST[@]})
+    echo '{"freelance": {"id": 42,"computedSkills":['
+    for k in "${!SKILL_LIST[@]}"
+    do
+	if [[ $k = ${KEYS[$((${#SKILL_LIST[@]}-1))]} ]] 
+	then
+            echo "{\"id\": ${SKILL_LIST[$k]},\"name\": \"$k\",\"durationInMonths\": ${SKILL_DURATION_LIST[$k]}}"
+	else
+            echo "{\"id\": ${SKILL_LIST[$k]},\"name\": \"$k\",\"durationInMonths\": ${SKILL_DURATION_LIST[$k]}},"
+        fi
+    done
+    echo ']}}'
+}
+
+get_all_skills_duration() {
+    var=$(declare -p "$1")
+    eval "declare -A _arr="${var#*=}
+    for k in "${!_arr[@]}"
+    do
+        get_skill_duration $k
     done
 }
 
@@ -70,13 +101,33 @@ get_skill_duration() {
         done
         i=$i+1
     done
-    SKILL_DURATION_LIST[$SKILL_NAME]=$(( ${SKILL_DURATION_LIST[$SKILL_NAME]}\
+    SKILL_DURATION_LIST[$SKILL_NAME]=$((\
+	    ${SKILL_DURATION_LIST[$SKILL_NAME]}\
 	    + $(compute_duration "EXP_DURATION_LIST")\
-	    + $(compute_overlap "EXP_DURATION_LIST") ))
+	    + $(compute_overlap "EXP_DURATION_LIST")\
+	    ))
+}
+
+get_skill_list() {
+    local i=0
+    while another_element .freelance.professionalExperiences[$i]
+    do
+	local j=0
+	while another_element .freelance.professionalExperiences[$i].skills[$j]
+	do
+	    local SKILL_NAME=$(jq -r ".freelance.professionalExperiences[$i].skills[$j].name" $INPUT_FILE)
+	    local SKILL_ID=$(jq -r ".freelance.professionalExperiences[$i].skills[$j].id" $INPUT_FILE)
+            SKILL_LIST[$SKILL_NAME]=$SKILL_ID
+	    SKILL_DURATION_LIST[$SKILL_NAME]=0
+	    j=$j+1
+	done
+        i=$i+1
+    done
 }
 
 print_associative_array() {
-    #Source : https://stackoverflow.com/questions/4069188/how-to-pass-an-associative-array-as-argument-to-a-function-in-bash
+    # this function is for debug purpose
+    # source : https://stackoverflow.com/questions/4069188/how-to-pass-an-associative-array-as-argument-to-a-function-in-bash
     var=$(declare -p "$1")
     eval "declare -A _arr="${var#*=}
     for k in "${!_arr[@]}"; do
@@ -84,45 +135,7 @@ print_associative_array() {
     done
 }
 
-compute_overlap() {
-    var=$(declare -p "$1")
-    eval "declare -A _arr="${var#*=}
-    local i=1
-    local OVERLAP_DURATION=0
-    KEYS=(${!_arr[@]})
-    while [[ i -lt ${#_arr[@]} ]]
-    do
-	OVERLAP=$(( ($(date --date=${KEYS[${i}]} +%s) - $(date --date=${_arr[${KEYS[$((i-1))]}]} +%s) )/(60*60*24*30) ))
-	if [[ $OVERLAP -lt 0 ]]
-	then
-		OVERLAP_DURATION=$((OVERLAP_DURATION + OVERLAP))
-	fi
-	i=$i+1
-    done
-    echo $OVERLAP_DURATION
-}
-
-compute_duration() {
-    var=$(declare -p "$1")
-    eval "declare -A _arr="${var#*=}
-    local i=0
-    local DURATION=0
-    KEYS=(${!_arr[@]})
-    while [[ i -lt ${#_arr[@]} ]]
-    do
-	DURATION=$(( $DURATION + (($(date --date=${_arr[${KEYS[$i]}]} +%s) - $(date --date=${KEYS[$i]} +%s) )/(60*60*24*30)) ))
-        i=$i+1
-    done
-    echo $DURATION
-}
-
 # MAIN
 get_skill_list
-get_skill_duration "Javascript"
-get_skill_duration "React"
-get_skill_duration "Java"
-get_skill_duration "Node.js"
-get_skill_duration "MySQL"
-print_associative_array "SKILL_LIST"
-print_associative_array "SKILL_DURATION_LIST"
-
+get_all_skills_duration "SKILL_LIST"
+display_result | jq .
